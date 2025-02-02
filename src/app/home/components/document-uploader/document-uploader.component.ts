6398e187fc331cb50d83be047489cc6b2521a0de
SebasMet <sebastian.metzger.nl@gmail.com>
2025-02-02 18:21:01 +0100

    Added document uploader component

---
+++ src/app/home/components/document-uploader/document-uploader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-uploader',
+  standalone: false,
+  
+  templateUrl: './document-uploader.component.html',
+  styleUrl: './document-uploader.component.css'
+})
+export class DocumentUploaderComponent {
+
+}
--- src/app/home/components/section-header/section-header.component.ts
+++ src/app/home/components/section-header/section-header.component.ts
@@ -1,11 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { provideIcons } from '@ng-icons/core';
-import { lucideChevronRight } from '@ng-icons/lucide';
+
 
 @Component({
   selector: 'app-section-header',
   standalone: false,
-  providers: [provideIcons({ lucideChevronRight })],
   templateUrl: './section-header.component.html',
   styleUrl: './section-header.component.css'
 })
